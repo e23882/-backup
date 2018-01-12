34322bbd98a9a573d8f46d2292e4cd58c14c5f0c
e23882@hotmail.com <801128>
2018-01-12 21:28:36 +0800

    土地政士網頁備份

---
+++ GlobalVars.php
@@ -0,0 +1,6 @@
+<?php
+$GLOBALS["serverName"]   = "sql100.byethost33.com";
+$GLOBALS["databaseName"] = "b33_19049243_yee";
+$GLOBALS["dbuser"]       = "b33_19049243";
+$GLOBALS["dbpassword"]   = "801128";
+?>
---
+++ QA.php
@@ -0,0 +1,16 @@
+<meta name="viewport" content="width=device-width, initial-scale= 1.0, user-scalable=no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta http-equiv="Content-Type" content=""text/html; charset=utf-8>
+<form  method="post" action ="saveQA.php" >
+	<div>
+		<input type="text" name="Q" placeholder="問題(新增?)" style="width:90%; font-size:14px; padding:6px 1%; margin-bottom:15px; background-color:#e6e6e6; border:1px solid #999; color:#666; outline:none; "/><br>
+		
+		<input type="text" name="title" placeholder="問題(ch標題?)"style="width:90%; font-size:14px; padding:6px 1%; margin-bottom:15px; background-color:#e6e6e6; border:1px solid #999; color:#666; outline:none; "/><br>
+	</div>
+	<div>
+		<textarea name="content" placeholder="內容" style="width:90%; font-size:14px; padding:6px 1%; margin-bottom:15px; background-color:#e6e6e6; border:1px solid #999; color:#666; outline:none; height:80px;"></textarea><br>
+	</div>
+	<div style="text-align:right; padding-right:10%;;">
+		<input type="submit" value="送出" style="width:20%; font-size:14px; padding:6px 1%; margin-bottom:15px; background-color:#f0f0f0; border:1px solid #999; color:#666; outline:none; "/>
+	</div>
+</form>
